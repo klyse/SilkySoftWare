fd0a3bb13da4da89127ebaf3211bc793db75ba9f
klyse <klaus.pruenster@gmail.com>
2022-11-11 19:27:54 +0100

    init

---
+++ SilkySoftWare/SilkySoftWareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilkySoftWareApp.swift
+//  SilkySoftWare
+//
+//  Created by Klaus Prünster on 2022-11-11.
+//
+
+import SwiftUI
+
+@main
+struct SilkySoftWareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
